b13ded9f9e15c3d2eee32ee2cc4fee0702d89a91
putriliza <putriliza05@gmail.com>
2022-08-19 22:09:46 +0700

    add routes and bootstrap mui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { useState, useEffect } from 'react'
-import './styles/App.css';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from "react-router-dom"
 
 import Projects from './components/list/Projects'
 import projectService from './services/projects'
 
+import { Navbar, Nav } from 'react-bootstrap'
+
+
+
+const Home = () => (
+  <div> <h2>HALOWWWW</h2> </div>
+)
+
+const Achievement = () => (
+  <div> <h2>MEONG MEONG</h2> </div>
+)
+
 const App = () => {
   const [projects, setProject] = useState([])
-
   useEffect(() => {
     projectService
       .getAll()
@@ -16,10 +30,37 @@ const App = () => {
   }, [])
 
   return (
-    <div className='App'>
-      <h1>Projects</h1>
-      <Projects projects={projects} />
+    <div className='container'>
+      <Router>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#" as="span">
+                <Link to="/">home</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link to="/projects">projects</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link to="/achievements">achievements</Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Routes>
+          <Route path="/projects" element={<Projects projects={projects} />} />
+          <Route path="/achievements" element={<Achievement />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <div>
+          <i>Portfoliza, miaw miaw, 2022</i>
+        </div>
+      </Router>
     </div>
+
   )
 }
 
